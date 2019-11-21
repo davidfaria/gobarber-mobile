6453e38785166ff3deef44dc23a0f086fbdf1ef3
David Faria <davidfaria89@gmail.com>
2019-11-21 10:31:30 -0300

    change baseUrl production

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
-import {BASE_URL} from 'react-native-dotenv';
+// import {BASE_URL} from 'react-native-dotenv';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  // baseURL: BASE_URL,
+  baseURL: 'http://api.gobarber.larawork.com.br',
 });
 
 export default api;
